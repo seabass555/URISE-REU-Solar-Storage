aaedad8ebfab20dc23e1b1cb25f2da1aa4134304
Sebastian <sharmstrong@umass.edu>
2021-06-17 00:30:01 -0400

    Addition of "main" file for Homestead Fl
    I migrated all of the code from "BESSIE" and implemented it in a new main file which includes commented sections for variable assignment, computation, and graphing. It still generates the same graphs.
    I also added a plot for the overloads if we upgraded the substation by 10MW.
    A few variables had name changes which I felt made more sense.
    Eg. netLoad -> netLoadSolar

--- calcLoadWithSolar.m
+++ calcLoadWithSolar.m
@@ -1,6 +1,7 @@
-function netLoad = calcLoadWithSolar(load,solarGen)
+function [netLoadSolar,solarGen] = calcLoadWithSolar(load,solarGen1MW, powerCap)
 %calculates the load with solar generation applied
-netLoad = load-solarGen;
-netLoad(netLoad<0) = 0;
+solarGen = solarGen1MW.*powerCap;
+netLoadSolar = load-solarGen;
+netLoadSolar(netLoadSolar<0) = 0;
 end
 
