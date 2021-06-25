2355dc9d8f7606ca709d1f7dd39f3aef5659e99d
amitchell5367 <amitchell5367@gmail.com>
2021-06-25 15:07:08 -0400

    Washington Update
    Simply changed the nameplate rating, and the size of the PV+BESS array.  It better fits the increased grid demand.

--- mainChelancountyWA2S.m
+++ mainChelancountyWA2S.m
@@ -19,10 +19,10 @@ time = time'; %time in hours for dataset
 deltaTime = 1; % time increment IN HOURS
 
 %BESS variables
-energyCapBESS = 100; % MWh maximum capacity
-initialEnergyBESS = 40; % MWh initial capacity
-chargePowerCap = 15; %MW BESS charge Power Cap
-dischargePowerCap = 15; %MW BESS discharge Power Cap
+energyCapBESS = 200; % MWh maximum capacity
+initialEnergyBESS = 80; % MWh initial capacity
+chargePowerCap = 30; %MW BESS charge Power Cap
+dischargePowerCap = 30; %MW BESS discharge Power Cap
 
 %use these for BESSFunc original:
 %chargeThreshold = 65; % load in MW at when BESS will charge
@@ -34,10 +34,10 @@ dischargePerc = 110; %percentage of mean load to discharge
 dischargeFactor = 50; %percentage for how much to bring down load to discharge threshold (0=none, 100=flat)
 
 
-arraySize = 40; % capacity of solar array in MW
+arraySize = 80; % capacity of solar array in MW
 
 %substation overload variables
-npCapacity = 90; %MW - nameplate rating of the substation transformer
+npCapacity = 300; %MW - nameplate rating of the substation transformer
 adjustmentFactorMax = 25; %percent maximum tolerable increase above substation rating
 adjustmentFactor = 0.4; %percent tolerable increase above rating for every percent the 24hr mean capacity factor is below 100%
 npCapacityInc = 10; %for potential substation upgrade - MW increase in nameplate capacity
@@ -74,98 +74,100 @@ percLoadGrowth = 5;
 
 %% Generate graphs
 
-% plotSolarBESSLoad(1,load,netLoadSolar,netLoadBESS,solarGen,powerOutBESS,1,'Yearly Net Loads and Solar, BESS Outputs');
-% plotSolarBESSLoad(2,load,netLoadSolar,netLoadBESS,solarGen,powerOutBESS,0,'Net Loads and Solar, BESS Outputs');
-% 
-% plotOverloads(3,load,npCapacity,npOverloadsBaseline,adjustedOverloadsBaseline,0,'Baseline Overloads');
-% plotOverloads(4,netLoadBESS,npCapacity,npOverloadsBESS,adjustedOverloadsBESS,0,'Overloads w/ Solar and BESS');
-% 
-% plotBESSData(5,netLoadBESS,powerOutBESS,energyBESS,0,'Solar, BESS Outputs');
+plotSolarBESSLoad(1,load,netLoadSolar,netLoadBESS,solarGen,powerOutBESS,1,'Yearly Net Loads and Solar, BESS Outputs');
+plotSolarBESSLoad(2,load,netLoadSolar,netLoadBESS,solarGen,powerOutBESS,0,'Net Loads and Solar, BESS Outputs');
+
+plotOverloads(3,load,npCapacity,npOverloadsBaseline,adjustedOverloadsBaseline,0,'Baseline Overloads');
+plotOverloads(4,netLoadBESS,npCapacity,npOverloadsBESS,adjustedOverloadsBESS,0,'Overloads w/ Solar and BESS');
+
+plotBESSData(5,netLoadBESS,powerOutBESS,energyBESS,0,'Solar, BESS Outputs');
 
 plotCosts(6,netCostsCO2BESS,netCostsCO2Upgrade,netCostsUSDBESS,netCostsUSDUpgrade,'Net Costs in C02','Net Costs in USD');
 
 
-% %graph of netload with just solar, netload with BESS+solar, solar generation, BESS power out
-% %subplot(4,1,1);
-% subplot(2,1,1);
-% hold on;
+%graph of netload with just solar, netload with BESS+solar, solar generation, BESS power out
+%subplot(4,1,1);
+subplot(2,1,1);
+hold on;
 % axis([4200 4500 -15 130]); %% parts of June and July only
-% %axis([0 8040 -15 130]);
-% plot(time,netLoadSolar, 'r');
-% plot(time,powerOutBESS,'k');
-% plot(time,netLoadBESS, 'g');
-% plot(time,solarGen, 'y');
-% xlabel('Time in hours after 12 am 1/1/20');
-% ylabel('MW');
-% legend('Load with Solar generation', 'BESS Power Output', 'Load with Solar and Storage', 'Solar Generation');
-% title('Net Loads and Solar, BESS Outputs');
-% xticks([0 744 1440 2184 2904 3648 4368 5112 5856 6576 7320 8040]);
-% xticklabels({'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'});
+axis([0 8040 -30 400]);
+plot(time,netLoadSolar, 'r');
+plot(time,powerOutBESS,'k');
+plot(time,netLoadBESS, 'g');
+plot(time,solarGen, 'y');
+xlabel('Time in hours after 12 am 1/1/20');
+ylabel('MW', FontSize, 12);
+legend('Load with Solar generation', 'BESS Power Output', 'Load with Solar and Storage', 'Solar Generation');
+title('Net Loads and Solar, BESS Outputs');
+xticks([0 744 1440 2184 2904 3648 4368 5112 5856 6576 7320 8040]);
+xticklabels({'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'});
 
-%graph for overloads with BESS+solar system (also display BESS energy)
-%subplot(4,1,2);
-%subplot(2,1,2);
-% colororder({'r','b'});
-% %axis([4200 4500 0 30]); %% parts of June and July only
-% hold on;
-% yyaxis left
-% axis([0 8800 0 28]);
-% plot(time, npOverloadsBESS, 'k');
-% plot(time, adjustedOverloadsBESS, 'r');
-% ylabel('MW');
-% yyaxis right
-% axis([0 8800 0 250]);
-% plot(time,energyBESS,'b'); %energy/10 to fit better on graph
-% xlabel('Time in hours after 12 am 1/1/20');
-% ylabel('MWh');
-% legend('Overloads above Nameplate Capacity (MW)', 'Overloads above Nameplate Capacity that may result in damage (MW)', 'Energy Stored in BESS (MWh)');
-% title('Overloads with Solar and BESS System');
-% xticks([0 744 1440 2184 2904 3648 4368 5112 5856 6576 7320 8040]);
-% xticklabels({'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'});
+% graph for overloads with BESS+solar system (also display BESS energy)
+subplot(4,1,2);
+subplot(2,1,2);
+colororder({'r','b'});
+%axis([4200 4500 0 30]); %% parts of June and July only
+hold on;
+yyaxis left
+axis([0 8800 0 28]);
+plot(time, npOverloadsBESS, 'k');
+plot(time, adjustedOverloadsBESS, 'r');
+ylabel('MW');
+yyaxis right
+axis([0 8800 0 250]);
+plot(time,energyBESS,'b'); %energy/10 to fit better on graph
+xlabel('Time in hours after 12 am 1/1/20');
+ylabel('MWh');
+legend('Overloads above Nameplate Capacity (MW)', 'Overloads above Nameplate Capacity that may result in damage (MW)', 'Energy Stored in BESS (MWh)');
+title('Overloads with Solar and BESS System');
+xticks([0 744 1440 2184 2904 3648 4368 5112 5856 6576 7320 8040]);
+xticklabels({'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'});
+FontSize = 20;
+FontWeight = 'bold';
 
 
 
-%graph for overloads without BESS+solar or upgrade
-% subplot(4,1,3);
-% hold on;
-% axis([0 8800 0 70]);
-% %axis([4200 4500 0 60]); %% parts of June and July only
-% plot(time, npOverloadsBaseline, 'k');
-% plot(time, adjustedOverloadsBaseline, 'r');
-% xlabel('Time in hours after 12 am 1/1/20');
-% ylabel('MW');
-% legend('Overloads above Nameplate Capacity (MW)', 'Overloads above Nameplate Capacity that may result in damage (MW)');
-% title('Overloads without Solar and BESS System or Upgrade');
-% xticks([0 744 1440 2184 2904 3648 4368 5112 5856 6576 7320 8040]);
-% xticklabels({'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'});
-% 
-% %graph for overloads with substation upgrade
-% subplot(4,1,4);
-% hold on;
-% axis([0 8800 0 45]);
-% %axis([4200 4500 0 45]); %% parts of June and July only
-% plot(time, npOverloadsUpgrade, 'k');
-% plot(time, adjustedOverloadsUpgrade, 'r');
-% xlabel('Time in hours after 12 am 1/1/20');
-% ylabel('MW');
-% legend('Overloads above Nameplate Capacity (MW)', 'Overloads above Nameplate Capacity that may result in damage (MW)');
-% title('Overloads with 10MW Substation Upgrade');
-% xticks([0 744 1440 2184 2904 3648 4368 5112 5856 6576 7320 8040]);
-% xticklabels({'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'});
+% graph for overloads without BESS+solar or upgrade
+subplot(4,1,3);
+hold on;
+axis([0 8800 0 70]);
+%axis([4200 4500 0 60]); %% parts of June and July only
+plot(time, npOverloadsBaseline, 'k');
+plot(time, adjustedOverloadsBaseline, 'r');
+xlabel('Time in hours after 12 am 1/1/20');
+ylabel('MW');
+legend('Overloads above Nameplate Capacity (MW)', 'Overloads above Nameplate Capacity that may result in damage (MW)');
+title('Overloads without Solar and BESS System or Upgrade');
+xticks([0 744 1440 2184 2904 3648 4368 5112 5856 6576 7320 8040]);
+xticklabels({'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'});
 
-%graph costs for CO2
-% hold on;
-% yyaxis left;
-% plot(netCostsCO2BESS, '--o');
-% plot(netCostsCO2Upgrade, 'k--o');
-% ylabel('Tons CO2');
-% yyaxis right;
-% plot(netCostsUSDBESS, 'g--*');
-% plot(netCostsUSDUpgrade, 'r--*');
-% ylabel('US Dollars')
-% xlabel('Years');
-% legend('CO2 emissions BESS','CO2 emissions subst. upgrade','USD costs BESS','USD costs subst. upgrade');
-% title('Costs in Net CO2 Emissions/USD');
+%graph for overloads with substation upgrade
+subplot(4,1,4);
+hold on;
+axis([0 8800 0 45]);
+%axis([4200 4500 0 45]); %% parts of June and July only
+plot(time, npOverloadsUpgrade, 'k');
+plot(time, adjustedOverloadsUpgrade, 'r');
+xlabel('Time in hours after 12 am 1/1/20');
+ylabel('MW');
+legend('Overloads above Nameplate Capacity (MW)', 'Overloads above Nameplate Capacity that may result in damage (MW)');
+title('Overloads with 10MW Substation Upgrade');
+xticks([0 744 1440 2184 2904 3648 4368 5112 5856 6576 7320 8040]);
+xticklabels({'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'});
+
+% graph costs for CO2
+hold on;
+yyaxis left;
+plot(netCostsCO2BESS, '--o');
+plot(netCostsCO2Upgrade, 'k--o');
+ylabel('Tons CO2');
+yyaxis right;
+plot(netCostsUSDBESS, 'g--*');
+plot(netCostsUSDUpgrade, 'r--*');
+ylabel('US Dollars')
+xlabel('Years');
+legend('CO2 emissions BESS','CO2 emissions subst. upgrade','USD costs BESS','USD costs subst. upgrade');
+title('Costs in Net CO2 Emissions/USD');
 
 
 
